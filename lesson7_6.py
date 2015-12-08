cdb54c072dc8c83e8aaa3c9e781ce8815a774065
backulinivan <backulinivan@gmail.com>
2015-12-08 11:02:57 +0300

    Update lesson7_6.py

--- lesson7_6.py
+++ lesson7_6.py
@@ -2,16 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-input=open('input.txt','r')
-date=input.read().split()
-date_len=[len(x) for x in date]
-max_len=max(date_len)
-gist_date=[0]*max_len
-for x in date_len:
-    gist_date[x-1]+=1
+wordFile = open('input.txt','r')
+words = wordFile.read().split()
+lengths = [len(x) for x in words]
+max_length = max(lengths)
+gist_lengths = [0]*(max(lengths)+1)
+for i in range(0, max_length+1):
+    gist_lengths[i] = lengths.count(i)
+gist_lengths = gist_lengths[1:]
 
-y_pos = [1+i for i in range(max_len)]
-plt.bar(y_pos, gist_date, align='center', alpha=0.8)
+y_pos = [1+i for i in range(max_length)]
+plt.bar(y_pos, gist_lengths, align='center', alpha=0.8)
 plt.xticks(y_pos, y_pos)
 plt.ylabel('Value')
 plt.title('Bar title')
